7d9d0a19d28ec5b3b136721c3447c0344ed18024
Pierre Dupont <pierre.dupont@kedgebs.com>
2023-10-13 14:10:15 +0200

    Quote seulement pour les sources

--- macros/snow-mask/apply-policy/apply_masking_policy_list_for_sources.sql
+++ macros/snow-mask/apply-policy/apply_masking_policy_list_for_sources.sql
@@ -62,11 +62,11 @@
                         {{ log(modules.datetime.datetime.now().strftime("%H:%M:%S") ~ " | " ~ operation_type ~ "ing masking policy to source : " ~ masking_policy_db|upper ~ '.' ~ masking_policy_schema|upper ~ '.' ~ masking_policy_name|upper ~ " on " ~ database ~ '.' ~ schema ~ '.' ~ identifier ~ '.' ~ column ~ ' [force = ' ~ var('use_force_applying_masking_policy','False') ~ ']', info=True) }}
                         {% set query %}
                             {% if operation_type == "apply" %}
-                                alter {{materialization}} "{{database}}"."{{schema}}"."{{identifier}}"
+                                alter {{materialization}} {{database}}.{{schema}}.{{identifier}}
                                 modify column "{{column}}"
                                 set masking policy  {{masking_policy_db}}.{{masking_policy_schema}}.{{masking_policy_name}} {% if conditional_columns | length > 0 %}using ({{column}}, {{conditional_columns|join(', ')}}){% endif %} {% if var('use_force_applying_masking_policy','False')|upper in ['TRUE','YES'] %} force {% endif %}
                             {% elif operation_type == "unapply" %}
-                                alter {{materialization}}  "{{database}}"."{{schema}}"."{{identifier}}" modify column  "{{column}}" unset masking policy
+                                alter {{materialization}}  {{database}}.{{schema}}.{{identifier}} modify column  "{{column}}" unset masking policy
                             {% endif %}
                         {% endset %}
                         {% do run_query(query) %}
